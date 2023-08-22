bd07adfdc8c1eb8a1a669d82ce4693e89742b748
WangXinpeng-666 <1980625438@qq.com>
2023-08-22 19:09:55 +0800

    first commit

--- helloword.java
+++ helloword.java
@@ -2,7 +2,8 @@ public class helloword
 {
     public static void main(String[] args)
     {
-        System.out.println("hello word");//输出hello word
+        System.out.println("hello word");
+        
        
     }
 }
